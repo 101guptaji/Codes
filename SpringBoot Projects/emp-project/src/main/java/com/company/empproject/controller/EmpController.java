51c385671f86a2e2b6689ba23600076e5a2ad68c
Himanshu Gupta <101guptaji@gmail.com>
2024-11-30 11:35:44 +0530

    EM-Project front-end
    EM-Project front-end using React.js and trailwind.css

--- SpringBoot Projects/emp-project/src/main/java/com/company/empproject/controller/EmpController.java
+++ SpringBoot Projects/emp-project/src/main/java/com/company/empproject/controller/EmpController.java
@@ -9,6 +9,7 @@ import com.company.empproject.services.EmployeeServiceImp;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 
 @RestController
+@CrossOrigin("http://localhost:3000/")
 public class EmpController {
     
     //EmployeeSerivce employeeSerivce = new EmployeeSerivceImpl();
